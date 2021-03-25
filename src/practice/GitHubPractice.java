78f45a27f8cc14150e9272380e9fec1100a47942
Hela <Hela@DESKTOP-IU83D1G>
2021-03-25 10:06:34 -0400

    commit is done

--- src/practice/GitHubPractice.java
+++ src/practice/GitHubPractice.java
@@ -1,8 +1,12 @@
 package practice;
 
 public class GitHubPractice {
-public static void main(String[] args) {
-	System.out.println(" Git Hub Day");
-			
+	public static void main(String[] args) {
+		System.out.println(" Git Hub Day");
+
+	}
+
+public static void divNum(int a , int b) {
+	System.out.println ("a , b");
 }
 }
